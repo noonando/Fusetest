f90972fd9dcffbf10bce57302bf0ee40d809fd91
Scott Pattersin <scott.patterson@clearlink.com>
2019-06-10 13:42:14 -0600

    1.02 dev

--- Scripts/Fuse/Encr/Script1559148142106.groovy
+++ Scripts/Fuse/Encr/Script1559148142106.groovy
@@ -52,7 +52,7 @@ WebUI.click(findTestObject('EncrFolder/Page_ Fuse (Scott Patterson)/button_Next 
 
 WebUI.delay(5)
 
-WebUI.closeBrowser()
+WebUI.waitForPageLoad(15)
 
-WebUI.waitForPageLoad(2)
+WebUI.closeBrowser()
 
